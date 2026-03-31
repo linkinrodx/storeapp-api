edc54d7c3958e2324b6cd0dff1bcfa06855104a5
Rodrigo <rodrigo.saraya@newmarketinc.com>
2026-03-31 00:44:02 -0500

    rever configuration

--- src/LambdaEntryPoint.cs
+++ src/LambdaEntryPoint.cs
@@ -1,23 +1,8 @@
 using Amazon.Lambda.AspNetCoreServer;
-using Amazon.Lambda.Core;
-using Amazon.Lambda.AspNetCoreServer.Internal;
-using Amazon.Lambda.RuntimeSupport;
-using Amazon.Lambda.Serialization.SystemTextJson;
-using System.Text.Json;
 
 namespace StoreApp.Api;
 
 public class LambdaEntryPoint : APIGatewayProxyFunction
-{
-    protected override void Init(IWebHostBuilder builder)
-    {
-        builder.UseStartup<Startup>();
-    }
-
-    protected override void Init(IHostBuilder builder) { }
-}
-
-public class FunctionUrlEntryPoint : Amazon.Lambda.AspNetCoreServer.FunctionUrlAWSCore.LambdaFunctionUrlController
 {
     protected override void Init(IWebHostBuilder builder)
     {
